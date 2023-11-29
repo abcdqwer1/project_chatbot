550a89e497cb9ed454d987ecb050899af849c04c
LeeCH <ckdgus2323@naver.com>
2023-11-29 18:00:16 +0900

    Add: accounts 회원가입

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=50, unique=True, null=False)
+    email = models.EmailField(unique=True)
+    nickname = models.CharField(max_length=16, default='')
+    profile_img = models.ImageField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
