1c1cdec2649400e35ffc82dd7069eb0d7f2e55da
LeeCH <ckdgus2323@naver.com>
2023-11-29 17:59:50 +0900

    Fix: main

--- main/models.py
+++ main/models.py
@@ -1,14 +1,10 @@
 from django.db import models
 
-class Message(models.Model):
-    user = models.CharField(max_length=100)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
 class Conversation(models.Model):
     prompt = models.CharField(max_length=512)
     response = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.prompt}: {self.response}"
--- main/serializers.py
+++ main/serializers.py
@@ -1,29 +1,22 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
-from .models import Message
 from .models import Conversation
 from django.contrib.auth import get_user_model
-
 
 # class UserSerializer(ModelSerializer):
 #     class Meta:
 #         model = get_user_model()
 #         fields = ['username', 'email']
 
-
-# class MessageSerializer(ModelSerializer):
-#     user = UserSerializer()
-#     class Meta:
-#         model = Message
-#         fields = [
-#             'id',
-#             'user',
-#             'content',
-#             'created_at',
-#             'updated_at',
-#         ]
-
 class ConversationSerializer(serializers.ModelSerializer):
+    # user = UserSerializer()
     class Meta:
         model = Conversation
-        fields = '__all__'
+        # fields = '__all__'
+        fields = [
+            'id',
+            'user',
+            'content',
+            'created_at',
+            'updated_at',
+        ]
